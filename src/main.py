5aa6d20788bfd7f8ad4a8782780903d27561c17f
AdrienRR <69118801+AdrienRR@users.noreply.github.com>
2024-03-02 15:41:55 +0100

    Standard DQN

--- src/main.py
+++ src/main.py
@@ -21,8 +21,26 @@ def seed_everything(seed: int = 42):
 if __name__ == "__main__":
     seed_everything(seed=42)
     # Initialization of the agent. Replace DummyAgent with your custom agent implementation.
-    agent = ProjectAgent()
-    agent.load()
+    config = {'nb_actions': 4,
+            'learning_rate': 0.001,
+            'gamma':0.99,
+            'buffer_size': 1000000,
+            'epsilon_min': 0.1,
+            'epsilon_max': 1.,
+            'epsilon_decay_period': 40000,
+            'epsilon_delay_decay': 1200,
+            'batch_size': 1024,
+            'gradient_steps': 5,
+            'update_target_strategy': 'ema',#'replace', # or 'ema'
+            'update_target_freq': 200,
+            'update_target_tau': 0.001,
+            'criterion': torch.nn.SmoothL1Loss(),
+            'nb_neurons': 256,
+            'device': torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+            'memory_length':5}
+
+    agent = ProjectAgent(config)
+    agent.load('qnetwork.pt')
     # Keep the following lines to evaluate your agent unchanged.
     score_agent: float = evaluate_HIV(agent=agent, nb_episode=1)
     score_agent_dr: float = evaluate_HIV_population(agent=agent, nb_episode=15)
